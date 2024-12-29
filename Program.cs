6a2d05662b20f2148d14838e8c39881fdad92296
chucker61 <meliksahmertcakir@hotmail.com>
2024-12-29 13:53:17 +0300

    updated

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using BlazorApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,4 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+await host.RunAsync();
